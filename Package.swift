b8d6dafe756e5d23ea1a8ad829f89f262f964e68
Quang Tran <tranquangbk56@gmail.com>
2026-04-29 15:00:57 +0700

    Release 1.0.4

--- Package.swift
+++ Package.swift
@@ -9,14 +9,35 @@ let package = Package(
     products: [
         .library(
             name: "AdsSDK",
-            targets: ["AdsSDK"]
+            targets: ["AdsSDK", "AdsSDKPackageSupport"]
         ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.3.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-user-messaging-platform.git", exact: "3.1.0"),
+        .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", exact: "13.6.2"),
+        .package(url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager.git", exact: "7.7.2"),
+        .package(url: "https://github.com/Mintegral-official/MintegralAdSDK-Swift-Package.git", exact: "8.1.1"),
+        .package(url: "https://github.com/bytedance/AdsGlobalPackage.git", exact: "7.9.0-release.8"),
     ],
     targets: [
         .binaryTarget(
             name: "AdsSDK",
-            url: "https://github.com/QuangTranSoict/AdsSDK/releases/download/1.0.3/AdsSDK.xcframework.zip",
-            checksum: "58bcde7cd1a2f62bc1e4bb07eacd8a6fd3c8710641ac50bb5e5c5ba5ce4676c9"
+            url: "https://github.com/QuangTranSoict/AdsSDK/releases/download/1.0.4/AdsSDK.xcframework.zip",
+            checksum: "ad91cc86956f5988caedb8c1f7f01518a625966e6b3710d6780091b397bab2ea"
+        ),
+        .target(
+            name: "AdsSDKPackageSupport",
+            dependencies: [
+                "AdsSDK",
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+                .product(name: "GoogleUserMessagingPlatform", package: "swift-package-manager-google-user-messaging-platform"),
+                .product(name: "AppLovinSDK", package: "applovin-max-swift-package"),
+                .product(name: "VungleAdsSDK", package: "vungleadssdk-swiftpackagemanager"),
+                .product(name: "MintegralAdSDK", package: "mintegraladsdk-swift-package"),
+                .product(name: "AdsGlobalPackage", package: "adsglobalpackage"),
+            ],
+            path: "Sources/AdsSDKPackageSupport"
         ),
     ]
 )
