7818d7bf2c7b18dd51c6d426682e5de0e680b671
Quang Tran <tranquangbk56@gmail.com>
2026-04-29 14:25:02 +0700

    Updated

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdsSDK",
-            url: "https://github.com/QuangTranSoict/leansoft-ads-sdk/releases/download/Release_ver_1.0.1/AdsSDK.xcframework.zip",
+            url: "https://github.com/QuangTranSoict/AdsSDK/releases/tag/1.0.1/AdsSDK.xcframework.zip",
             checksum: "d1df8caa7d5341feb6ca3590f585d3873cdda7ff1a1705e3bd5069ea7f197158"
         ),
     ]
